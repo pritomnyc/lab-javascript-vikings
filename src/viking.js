e3691eba316deaa42c777c87f7d861b229a8d4c2
Mahim Pritom <mahimnyc@yahoo.com>
2021-06-16 22:50:10 -0400

    iteration#1-3 modified

--- src/viking.js
+++ src/viking.js
@@ -9,12 +9,23 @@ class Soldier {
     //console.log('Odin is with us');
     return this.strength;
   };
-  receiveDamage = (theDamage) => {
-    //console.log('Shield Wall!');
+  // receiveDamage = (theDamage) => {
+  //   //console.log('Shield Wall!');
+  //   this.health -= theDamage;
+  // };
+  receiveDamage(theDamage) {
+    console.log('Damage');
     this.health -= theDamage;
-  };
+    return this.health;
+  }
 }
+let Percy = new Soldier(50, 50);
+let Achilles = new Soldier(30, 60);
+
+Achilles.receiveDamage(Percy.attack());
+console.log(Percy, Achilles);
 //let soldierObj = new Soldier(100, 100);
+
 // Viking
 class Viking extends Soldier {
   constructor(name, health, strength) {
@@ -35,19 +46,34 @@ class Viking extends Soldier {
     return 'Odin Owns You All!';
   };
 }
+
+let Bjorn = new Viking('Bjorn', 3000, 100);
+console.log(Bjorn.battleCry());
+console.log(Bjorn.receiveDamage(Percy.attack()));
+console.log(Bjorn);
 //let vikingObj = new Viking('Bjorn Ironside', 1000, 1000);
 
 // Saxon
 class Saxon extends Soldier {
   receiveDamage = (theDamage) => {
-    this.health -= theDamage;
-    if (this.health > 0) {
-      return `A Saxon has received ${theDamage} points of damage`;
-    } else {
+    let health = super.receiveDamage(theDamage);
+    console.log(health);
+
+    if (health <= 0) {
       return `A Saxon has died in combat`;
+    } else {
+      return `A Saxon has received ${this.theDamage} points of damage`;
     }
+    // this.health -= theDamage;
+    // if (this.health > 0) {
+    //   return `A Saxon has received ${theDamage} points of damage`;
+    // } else {
+    //   return `A Saxon has died in combat`;
+    // }
   };
 }
+let Alfred = new Saxon(150, 40);
+console.log(Alfred.receiveDamage(Bjorn.attack()));
 
 // War
 class War {
@@ -55,9 +81,13 @@ class War {
     this.vikingArmy = [];
     this.saxonArmy = [];
   }
-  addViking = () => {};
+  addViking = (viking) => {
+    this.vikingArmy.push();
+  };
 
-  addSaxon = () => {};
+  addSaxon = (saxon) => {
+    this.saxonArmy.push();
+  };
 
   vikingAttack = () => {};
 
